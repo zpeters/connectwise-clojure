104c6ded1700c9aeb4666c77ef53ee6ad4c1aeef
Zach Peters <zpeters@gmail.com>
2020-11-13 10:48:51 -0600

    bare init

---
+++ src/connectwise/core.clj
@@ -0,0 +1,6 @@
+(ns connectwise.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/connectwise/core_test.clj
@@ -0,0 +1,7 @@
+(ns connectwise.core-test
+  (:require [clojure.test :refer :all]
+            [connectwise.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
