e9ed8217b92e879a83818900a11faec628e1af74
Zach Peters <zpeters@gmail.com>
2020-11-13 14:20:21 -0600

    update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.zpeters/connectwise "0.1.0"
+(defproject org.clojars.zpeters/connectwise "0.1.1"
   :description "Connectwise Manage interface"
   :url "https://github.com/zpeters/connectwise-clojure"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
